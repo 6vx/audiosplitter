e539e2d6bcaf4f649e587c99f6b8eed96358cb04
Tim Godin <support@kitebot.ca>
2021-06-26 19:47:07 -0600

    I dunno lets see

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Audio Splitter
+
+My Sony voice recorder has a lot of bells and whistles, but I still need to do some of the legwork myself. 
+
+Making this to derive timestamps based on the file's metadata (birthtime) and the accompanying .tmk files.
+
---
+++ zipper.js
@@ -0,0 +1,14 @@
+var ffmpeg = require('ffmpeg');
+
+try {
+	new ffmpeg('/path/to/your_movie.avi', function (err, video) {
+		if (!err) {
+			console.log('The video is ready to be processed');
+		} else {
+			console.log('Error: ' + err);
+		}
+	});
+} catch (e) {
+	console.log(e.code);
+	console.log(e.msg);
+}
